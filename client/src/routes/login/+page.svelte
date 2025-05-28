e843a293648ed233d31091b732b60b3072e6109b
saugat23 <saugatbhandari8@gmail.com>
2025-05-28 23:54:33 +0545

    login page

--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -1,1 +1,21 @@
-<h1 class="text-6xl text-white bg-red-500 hover:bg-blue-400 p-2 rounded-full">Login page</h1>
+<script>
+import logo from "../../../static/login/login-bg.png"
+</script>
+
+<div class="min-h-full h-screen w-screen flex justify-center items-center bg-linear-to-r from-[#1c4f4a] to-bg-stone-100 text-sm text-white">
+	<div class="bg-stone-100 border-3 border-solid border-[#1c4f4a] min-h-96 min-w-96 w-auto h-auto flex rounded-2xl">
+		<div class="bg-stone-100 w-1/2 h-full min-h-96 rounded-l-2xl flex justify-center items-center">
+			<img src="/login/login-bg.png" class="w-full h-full rounded-2xl" alt="Login bg" />
+		</div>
+		<div class="bg-[#1c4f4a] w-1/2 h-full min-h-96 rounded-r-2xl flex flex-col justify-center items-center p-10 space-y-5">
+			<h3 class="text-4xl text-extrabold text-white">Login</h3>
+			<input type="email" name="email" id="email" placeholder="Email address" class="p-2 rounded-lg border-1 border-solid border-stone-100 outline-none" />
+<input type="email" name="email" id="email" placeholder="Email address" class="p-2 rounded-lg border-1 border-solid border-stone-100 outline-none" />
+		</div>
+	</div>
+</div>
+
+
+<style lang="postcss">
+@reference "tailwindcss";
+</style>
