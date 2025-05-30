68d0af32e42a3595113f7a6aa49b5cb5bae9a79c
saugat23 <saugatbhandari8@gmail.com>
2025-05-30 22:47:32 +0545

    server

--- server/main.py
+++ server/main.py
@@ -1,15 +1,6 @@
-from typing import Union
-
-from fastapi import FastAPI
-
-app = FastAPI()
+def main():
+    print("Hello from server!")
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+if __name__ == "__main__":
+    main()
